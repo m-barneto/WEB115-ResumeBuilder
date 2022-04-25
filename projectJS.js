0eafa81d9dc69e699e53129e0278e180f1e4377e
mBarneto <4347791+m-barneto@users.noreply.github.com>
2022-04-25 15:19:31 -0400

    Worked on generated resume styling, and created a temporary html doc to test the layout

--- projectJS.js
+++ projectJS.js
@@ -3,7 +3,7 @@ function getValue(id) {
 }
 
 document.getElementById("submit").addEventListener("click", generateResume);
-
+fillFields();
 function generateResume() {
   if (!document.getElementById("email").validity.valid) return;
   let name = getValue("name");
@@ -11,6 +11,8 @@ function generateResume() {
   let phone = getValue("phone");
   let linkedin = getValue("linkedin");
   let portfolio = getValue("portfolio");
+  let picture = getValue("picture");
+  console.log(picture);
   let state = getValue("state");
   let city = getValue("city");
   let zip = getValue("zip");
@@ -51,7 +53,24 @@ function generateResume() {
 
   let references = getValue("references").toString().split('\n');
 
-  console.log(graddate1);
+  let flyWindow = window.open('about:blank', 'myPop');
+  let myText = `
+  <!DOCTYPE html>
+  <html>
+  
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>WEB-115 Final Project</title>
+    <link rel="stylesheet" type="text/css" href="resumestyle.css" />
+  </head>
+  
+  <body>
+    <img src="${picture}"></img>
+  </body>
+  
+  </html>
+  `;
+  flyWindow.document.write(myText);
 }
 
 document.getElementById("fillfields").addEventListener("click", fillFields);
@@ -62,6 +81,7 @@ function fillFields() {
   document.getElementById("phone").value = "(919) 895-5453";
   document.getElementById("linkedin").value = "https://www.linkedin.com/in/matthew-barneto-013bb2232";
   document.getElementById("portfolio").value = "https://m-barneto.github.io/";
+  document.getElementById("picture").value = "https://drive.google.com/uc?id=1uYwFmerP8ov7vfTBt4XNjNBVC3hpUwc3";
   document.getElementById("state").value = "NC";
   document.getElementById("city").value = "Raleigh";
   document.getElementById("zip").value = "27123";
@@ -69,13 +89,13 @@ function fillFields() {
 
 
   document.getElementById("objective").value =
-    ```Current student at Wake Tech Community College. I am pursuing an associates in programming and development with a specialization in C++ and Java. 
-    I'm looking for a job in software development but have experience with projects in a multitude of environments and stacks.```;
+    `Current student at Wake Tech Community College. I'm pursuing an associates in programming and development with a specialization in C++ and Java. 
+    I'm looking for a job in software development but have experience with projects in a multitude of environments and stacks.`;
   document.getElementById("devskills").value =
-    ```Hardworking, with a desire to create something I can be proud to show and represent.\n
+    `Hardworking, with a desire to create something I can be proud to show and represent.\n
     Love trying new frameworks and languages and recreating/improving upon old projects to become familiar in new environments.\n
     Familiar in Unix, Windows, and tinkering with microcontrollers like the Arduino and Teensy 3.
-    ```;
+    `;
   document.getElementById("project").value = "Particle system that uses Perlin noise to drive the movement of particles. Forming groups of particles that follow similar paths. Each particle paints a small amount onto the canvas to create a string-like field.";
   document.getElementById("projectlink").value = "https://www.youtube.com/watch?v=8_zC0eIjtkM";
 
@@ -90,15 +110,15 @@ function fillFields() {
 
   document.getElementById("school2").value = "Wake Tech Community College";
   document.getElementById("schoollocation2").value = "Raleigh";
-  document.getElementById("graddate2").value = "Current";
+  document.getElementById("graddate2").value = "";
 
 
   document.getElementById("certs").value = "SoloLearn: C++, C#";
 
 
   document.getElementById("job1").value = "Coffee Shop";
-  document.getElementById("entrydate1").value = "2020-4-4";
-  document.getElementById("exitdate1").value = "Current";
+  document.getElementById("entrydate1").value = "2020-04-04";
+  document.getElementById("exitdate1").value = "";
   document.getElementById("jobdesc1").value = "Working as a barista I've developed a love for creating things with my hands. As well as creating and maintaining relationships with customers.";
 
   document.getElementById("job2").value = "";
